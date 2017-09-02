138ab65d0b31eee4067a5352414e74c146872abd
Gor-Ren <gor.rennie@gmail.com>
2017-09-02 22:15:26 +0100

    initial commit

---
+++ Level 1/reverse_string.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  2 22:13:54 2017
+
+@author: Gor-Ren
+"""
+
+
+def reverse_string(a_string):
+    """Reverses an input string. Embarrassingly simple in Python."""
+    return a_string[::-1]
