4ad4aedb4ee7c6127e0aad103eaa8a404608db92
Gor-Ren <gor.rennie@gmail.com>
2017-09-05 22:10:51 +0100

    docstring update and linting

--- Level 2/number_of_leaves.py
+++ Level 2/number_of_leaves.py
@@ -5,12 +5,31 @@ Created on Tue Sep  5 19:12:22 2017
 @author: Gor-Ren
 """
 
+
 class BinaryTree:
+    """A binary tree with method to determine its number of leaves.
+
+    Attributes:
+        root: A TreeNode, the root of the binary tree
+    """
+
     def __init__(self, root_node=None):
+        """Inits BinaryTree with root node."""
         self.root = root_node
 
     def number_of_leaves(self, root):
+        """Calculates the number of leaves in the binary tree.
 
+        A leaf is a node with no children.
+
+        Args:
+            root: A TreeNode; the root of the tree to be analysed. Does not need
+                to match the root of the BinaryTree instance.
+
+        Returns:
+            An integer, the number of leaves in the binary tree descending from
+            root.
+        """
         if root is None:
             return 0
         elif root.left_child is None and root.right_child is None:
@@ -22,7 +41,8 @@ class BinaryTree:
 
 ## Class code provided by firecode.io for challenge
 class TreeNode:
-    def __init__(self, data,left_child = None, right_child = None):
+    """firecode.io implementation of a binary tree node."""
+    def __init__(self, data, left_child=None, right_child=None):
         self.data = data
         self.left_child = left_child
         self.right_child = right_child
