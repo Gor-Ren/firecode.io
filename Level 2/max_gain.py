78670105e063a3c482a2f125ab128ee19242c41d
Gor-Ren <gor.rennie@gmail.com>
2017-09-11 14:13:35 +0100

    added O(n) solution

--- Level 2/max_gain.py
+++ Level 2/max_gain.py
@@ -9,6 +9,33 @@ Created on Mon Sep 11 14:02:45 2017
 def max_gain(input_list):
     """Calculates the maximum gain between two elements in a list of ints.
 
+    An implementation which traverses the list only once to achieve O(n) time complexity.
+
+    Maximum gain is defined as the maximum difference between two elements in a list
+    such that the larger element appears after the smaller element. If no gain is possible,
+    returns 0.
+
+    Args:
+        input_list: A list of ints.
+
+    Returns:
+        An integer, the maximum gain between two elements in input_list
+    """
+    smallest = input_list[0]
+    max_g = 0
+
+    for num in input_list[1:]:
+        if num - smallest > max_g:
+            max_g = num - smallest
+        elif num < smallest:
+            smallest = num
+
+    return max_g
+
+
+def max_gain1(input_list):
+    """Calculates the maximum gain between two elements in a list of ints.
+
     A brute force implementation with O(n^2) time complexity.
 
     Maximum gain is defined as the maximum difference between two elements in a list
