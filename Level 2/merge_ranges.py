f2e85730dfd8e4e74e1d47dedeaac4f35d322474
Gor-Ren <gor.rennie@gmail.com>
2017-09-13 00:06:21 +0100

    add new algorithm - WIP
    New O(nlogn) implementation added, but needs work to catch edge cases where one or two ranges in input list.

--- Level 2/merge_ranges.py
+++ Level 2/merge_ranges.py
@@ -7,7 +7,68 @@ Created on Tue Sep 12 22:08:19 2017
 
 
 def merge_ranges(input_range_list):
+    """Merges overlapping Range objects.
 
+    When provided an input list of Ranges, identifies Ranges which overlap and
+    merges them into a single Range. Returns the merged list.
+
+    A single-pass solution which preprocesses input by sorting to achieve
+    O(nlogn) time complexity.
+
+    Args:
+        input_range_list: A list of Ranges to be merged.
+
+    Returns:
+        A list of Ranges, with overlapping Ranges merged.
+    """
+    # sorting the list in order of lower bounds allows us to merge in single
+    #  pass. Sorting in reverse order allows merged ranges to be deleted from
+    #  end of list in O(1) time.
+    rng_stack = sorted(input_range_list,
+                           key=lambda range_el: range_el.lower_bound,
+                           reverse=True)
+
+    result = []
+
+    while rng_stack:
+        if len(rng_stack) == 1: # edge case: can't merge single range
+            result.append(rng_stack[0])
+            break
+
+        merged_rng = rng_stack.pop()
+
+        while True:
+            if rng_stack[-1].upper_bound <= merged_rng.upper_bound:
+                # range is completely encompassed by merged_range; delete it
+                del rng_stack[-1]
+            elif rng_stack[-1].lower_bound <= merged_rng.upper_bound:
+                # range overlaps merged_range and extends it; merge upper range
+                merged_rng.upper_bound = rng_stack[-1].upper_bound
+                del rng_stack[-1]
+            else:
+                # ranges no longer overlap, break and add to result
+                break
+
+        result.append(merged_rng)
+
+    return result
+
+
+## Alternative solution - brute force
+def merge_ranges1(input_range_list):
+    """Merges overlapping Range objects.
+
+    When provided an input list of Ranges, identifies Ranges which overlap and
+    merges them into a single Range. Returns the merged list.
+
+    A naive implementation with O(n^2) time complexity.
+
+    Args:
+        input_range_list: A list of Ranges to be merged.
+
+    Returns:
+        A list of Ranges, with overlapping Ranges merged.
+    """
     range_list = input_range_list[:]
     result = []
 
@@ -20,7 +81,7 @@ def merge_ranges(input_range_list):
                 range1.upper_bound >= range2.upper_bound) )
 
     while range_list:
-        if len(range_list) == 1: # edge case: single range in list, nothing to merge
+        if len(range_list) == 1: # edge case: cannot merge single Range
             result.append(range_list.pop())
             break
 
@@ -44,3 +105,14 @@ def merge_ranges(input_range_list):
             del range_list[i]
 
     return result
+
+
+## Class code provided by firecode.io for challenge
+class Range(object):
+
+    def __init__(self, lower_bound=-1, upper_bound=-1):
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+
+    def __str__(self):
+        return "["+str(self.lower_bound)+","+str(self.upper_bound)+"]"
