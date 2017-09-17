c7b8715f3b2e754712c38d35d60d2fc20c2f3f6d
Gor-Ren <gor.rennie@gmail.com>
2017-09-17 15:29:50 +0100

    new solution
    added pointer-based solution with better memory complexity

--- Level 1/is_palindrome.py
+++ Level 1/is_palindrome.py
@@ -22,3 +22,35 @@ def is_palindrome(input_string):
         return is_palindrome(input_string[1:-1])
     else:
         return False
+
+
+## Alternative solution; better memory complexity, slightly more LOC
+def is_palindrome2(input_string):
+    """Determines whether a string is a palindrome.
+
+    This implementation passes a recursive helper function pointers to elements
+    in the string to be tested. This achieves constant memory complexity,
+    whereas passing substrings recusively would give quadratic memory
+    complexity.
+
+    Args:
+        input_string: The string to be tested.
+
+    Returns:
+        A boolean, whether the string is a palindrome.
+    """
+    def palindrome_helper(left, right):
+        """Helper. Uses pointers to recursively check whether input_string
+        variable in parent scope is a palindrome, returning bool."""
+        if not left < right:
+            return True
+        elif input_string[left] != input_string[right]:
+            return False
+        else:
+            return palindrome_helper(left+1, right-1)
+
+    # initialise pointers
+    left = 0
+    right = len(input_string)-1
+
+    return palindrome_helper(left, right)
