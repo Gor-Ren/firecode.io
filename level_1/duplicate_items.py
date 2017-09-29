dc3e212bc0ddcfbd10475e467f2215f8c8a6c812
Gor-Ren <gor.rennie@gmail.com>
2017-09-29 21:47:55 +0100

    new duplicate_items solution

--- level_1/duplicate_items.py
+++ level_1/duplicate_items.py
@@ -6,6 +6,7 @@ Created on Mon Sep  4 15:45:11 2017
 """
 
 
+# A single-pass solution with O(nlogn) time complexity arising from sorting.
 def duplicate_items(list_numbers):
     """Checks a list of ints and returns a list containing its duplicates.
 
@@ -16,7 +17,6 @@ def duplicate_items(list_numbers):
         A list, the duplicate elements.
     """
     duplicates = []
-
     sorted_numbers = sorted(list_numbers)
 
     # The list is sorted, therefore an element only needs to be tested against
@@ -26,3 +26,19 @@ def duplicate_items(list_numbers):
             duplicates.append(sorted_numbers[i])
 
     return duplicates
+
+
+# Alternative solution: utilises a set and sorts at end of function instead of
+#  start. Greater space complexity (linear) but will run faster when duplicates
+#  only represent a minority of the inputs due to less elements to sort.
+def duplicate_items2(list_numbers):
+    seen = set()
+    duplicates = []
+
+    for num in list_numbers:
+        if num in seen:
+            duplicates.append(num)
+        else:
+            seen.add(num)
+
+    return sorted(duplicates)
